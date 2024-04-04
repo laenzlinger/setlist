ba6b092740b2dc65ba052b0167802fa8112948b3
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2024-04-04 22:32:32 +0200

    Move song header into separate file

--- internal/song/song.go
+++ internal/song/song.go
@@ -22,35 +22,3 @@ func (s Song) RemoveColumns(idx Indexes) Song {
 	s.TableRow = RemoveCols(idx, s.TableRow)
 	return s
 }
-
-type Header struct {
-	tableHeader *ast.Node
-}
-
-func NewHeader(h *ast.Node) Header {
-	return Header{tableHeader: h}
-}
-
-func (h Header) Remove() Header {
-	h.tableHeader = nil
-	return h
-}
-
-func (h Header) RemoveColumns(idx Indexes) Header {
-	if !h.Empty() {
-		removed := RemoveCols(idx, *h.tableHeader)
-		h.tableHeader = &removed
-	}
-	return h
-}
-
-func (h Header) Empty() bool {
-	return h.tableHeader == nil
-}
-
-func (h Header) Node() ast.Node {
-	if h.Empty() {
-		return ast.NewText()
-	}
-	return *h.tableHeader
-}
